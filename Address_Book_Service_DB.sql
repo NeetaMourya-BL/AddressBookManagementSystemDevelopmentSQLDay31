15f2bc13ccc6ef838547c61804ac42002da6f56e
unknown <neetamourya24@gmail.com>
2021-12-27 23:36:39 +0530

    [Neeta mourya] create databse in createDatabase UC1 branch

--- Address_Book_Service_DB.sql
+++ Address_Book_Service_DB.sql
@@ -1,1 +1,6 @@
 show databases;
+
+UC1
+create database Address_Book_Service_DB;
+use Address_Book_Service_DB;
+select database();
--- Address_Book_Service_DBInput.sql
+++ Address_Book_Service_DBInput.sql
@@ -1,1 +1,6 @@
 show databases;
+create database Address_Book_Service_DB;
+use Address_Book_Service_DB;
+select database();
+drop database Address_Book_Service_DB;
+show databases;
