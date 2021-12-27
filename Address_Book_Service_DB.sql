d9503da65403c1aa3b82b58b3b6bdb120f3eb53a
unknown <neetamourya24@gmail.com>
2021-12-27 23:47:39 +0530

    [Neeta mourya] insert contact into address book in insertContact UC3 branch

--- Address_Book_Service_DB.sql
+++ Address_Book_Service_DB.sql
@@ -9,3 +9,8 @@ UC2
 create table Address_Book(fName varchar(20), lName varchar(20), address varchar(20), city varchar (20), state varchar (20), zip varchar (10), phoneNumber varchar(15),email varchar(25));
 describe tables;
 drop table Address_Book;
+
+UC3
+insert into Address_Book(fName,lName,address,city,state,zip,phoneNumber,email) values('Neeta','Mourya','Dayalband','Bsp','CG',495001,9875235232,'neeta@gmail.com');
+insert into Address_Book(fName,lName,address,city,state,zip,phoneNumber,email) values('Prakash','kashyap','Dayalband','Bsp','CG',495001,9875235232,'prakash@gmail.com');
+select * from Address_Book;
--- Address_Book_Service_DBInput.sql
+++ Address_Book_Service_DBInput.sql
@@ -5,3 +5,7 @@ select database();
 drop database Address_Book_Service_DB;
 show databases;
 create table Address_Book(fName varchar(20), lName varchar(20), address varchar(20), city varchar (20), state varchar (20), zip varchar (10), phoneNumber varchar(15),email varchar(25));
+drop table Address_Book;
+insert into Address_Book(fName,lName,address,city,state,zip,phoneNumber,email) values('Neeta','Mourya','Dayalband','Bsp','CG',495001,9875235232,'neeta@gmail.com');
+insert into Address_Book(fName,lName,address,city,state,zip,phoneNumber,email) values('Prakash','kashyap','Dayalband','Bsp','CG',495001,9875235232,'prakash@gmail.com');
+select * from Address_Book;
